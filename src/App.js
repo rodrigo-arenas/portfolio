03393212a5328d85d153f41da3caa6970de97bd2
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2021-12-07 09:54:00 -0500

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
