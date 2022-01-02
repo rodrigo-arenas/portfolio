d00049cd9540e95932c88246edd9063be1e7377f
Dian Jonker <dianjonker@gmail.com>
2022-01-02 20:28:39 +0200

    Change footer layout and class names, and add hover to social icons

--- src/components/Footer/FooterItems.js
+++ src/components/Footer/FooterItems.js
@@ -4,20 +4,20 @@ import "./Footer.css"
 
 const FooterItems = () => {
     return (
-        <ul className={"social-icons"}>
+        <div className={"social-icons"}>
             {footerConfig.icons.map((item) => (
-                    <li className={item.className} key={item.id}>
+                    <div className={item.className} key={item.id}>
                         <a href={item.url}
                            style={{color: "#f5f5f5"}}
                            target={item.target}
                            rel={"noopener noreferrer"}>
                             {item.icon}
                         </a>
-                    </li>
+                    </div>
 
                 )
             )}
-        </ul>
+        </div>
     )
 
 }
