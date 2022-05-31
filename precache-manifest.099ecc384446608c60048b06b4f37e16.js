887e3472c3ceff3731c0580208ba7851472ebcf8
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2022-05-30 19:11:34 -0500

    Updates

---
+++ precache-manifest.099ecc384446608c60048b06b4f37e16.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd702e39f99674633dc1",
+    "url": "/portfolio/static/css/main.d507fa8c.chunk.css"
+  },
+  {
+    "revision": "fd702e39f99674633dc1",
+    "url": "/portfolio/static/js/main.fd702e39.chunk.js"
+  },
+  {
+    "revision": "1c849c641521a85365f5",
+    "url": "/portfolio/static/css/1.eefa6ff0.chunk.css"
+  },
+  {
+    "revision": "1c849c641521a85365f5",
+    "url": "/portfolio/static/js/1.1c849c64.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "/portfolio/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "eadebb0e5fa4ba86c743e56c5639b042",
+    "url": "/portfolio/index.html"
+  }
+];
