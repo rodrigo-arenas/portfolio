c0043cac30faeeb1a0af1d613b573603ccc1456c
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2022-07-02 10:48:11 -0500

    add Scheduling article

--- src/assets/configs/blogConfig.js
+++ src/assets/configs/blogConfig.js
@@ -5,6 +5,20 @@ import fastapiSklearn from "../images/fastapi_sklearn.png"
 
 const blogConfig = [
     {
+        id: "blog-8",
+        title: "How to Solve Scheduling Problems in Python",
+        links: [
+            {
+                name: "article",
+                url: "https://towardsdatascience.com/how-to-solve-scheduling-problems-in-python-36a9af8de451",
+                icon: <SiMedium/>
+            }
+        ],
+        image: "https://miro.medium.com/max/700/1*VKIGzmJrYBzcPlB6USx8OA.jpeg",
+        description: "Use linear programming to minimize the difference between required and scheduled resources.",
+        target: "_blank"
+    },
+    {
         id: "blog-7",
         title: "Adaptive Parameters Methods for Machine Learning",
         links: [
