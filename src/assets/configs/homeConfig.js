f5eb377d7a71da9c681a6ea83d60b36d6a61db75
Abhimanyu Sangitrao <abhi.sangitrao47@gmail.com>
2023-10-01 17:32:47 +0100

    minor changes to home page (test commit)

--- src/assets/configs/homeConfig.js
+++ src/assets/configs/homeConfig.js
@@ -8,14 +8,13 @@ const homeConfig = {
 
     greeting:
         <h1 className="heading">
-            Hi! I'm <strong className="main-name"> Rodrigo Arenas</strong>
+            Hi! I'm <strong className="main-name"> Abhimanyu Sangitrao</strong>
         </h1>,
     titles: [
         "Data Scientist",
         "Machine Learning Specialist",
         "Open Source Contributor",
-        "Backend Developer",
-        "Colombian"
+        "Python Backend Developer",
     ],
     about: {
         start: "I've been working for over six years on data science projects. " +
