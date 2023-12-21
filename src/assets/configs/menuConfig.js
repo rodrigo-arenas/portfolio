c46e5efb100ffb8892f1a08f71e2488d3aa3d5ea
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-12-21 19:32:03 +0500

    dark mode functionility added

--- src/assets/configs/menuConfig.js
+++ src/assets/configs/menuConfig.js
@@ -1,47 +1,40 @@
 import React from "react";
 
-import {
-    AiFillCode,
-    AiFillHome,
-    AiOutlineGithub
-} from "react-icons/ai";
+import { AiFillCode, AiFillHome, AiOutlineGithub } from "react-icons/ai";
 
-import {BsMedium} from "react-icons/bs";
-
+import { BsMedium } from "react-icons/bs";
 
 const menuConfig = {
+  sidebarData: [
+    {
+      id: "menu-0",
+      title: "Home",
+      path: "/portfolio",
+      icon: <AiFillHome size={35} />,
+      className: "nav-text",
+    },
+    {
+      id: "menu-1",
+      title: "Projects",
+      path: "/projects",
+      icon: <AiOutlineGithub size={35} />,
+      className: "nav-text",
+    },
+    {
+      id: "menu-2",
+      title: "Blogs",
+      path: "/blogs",
+      icon: <BsMedium size={35} />,
+      className: "nav-text",
+    },
+    {
+      id: "menu-3",
+      title: "Skills",
+      path: "/skills",
+      icon: <AiFillCode size={35} />,
+      className: "nav-text",
+    },
+  ],
+};
 
-    sidebarData: [
-        {
-            id: "menu-0",
-            title: "Home",
-            path: "/portfolio",
-            icon: <AiFillHome size={35}/>,
-            className: "nav-text"
-        },
-        {
-            id: "menu-1",
-            title: "Projects",
-            path: "/projects",
-            icon: <AiOutlineGithub size={35}/>,
-            className: "nav-text"
-        },
-        {
-            id: "menu-2",
-            title: "Blogs",
-            path: "/blogs",
-            icon: <BsMedium size={35}/>,
-            className: "nav-text"
-        },
-        {
-            id: "menu-3",
-            title: "Skills",
-            path: "/skills",
-            icon: <AiFillCode size={35}/>,
-            className: "nav-text"
-        }
-    ]
-
-}
-
-export default menuConfig
+export default menuConfig;
