84bb718d298c268e712fc0e7bedd9c95b6d82d15
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-12-28 01:50:03 +0500

    optimize code

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,39 +1,35 @@
 import React from "react";
 import {
-    VerticalTimeline, VerticalTimelineElement
+  VerticalTimeline,
+  VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Tags from "../Tags/Tags";
-import "./Timeline.css"
-
+import "./Timeline.css";
 
 const Timeline = (props) => {
-    return (
-        <VerticalTimeline>
-            {props.items.map((item) => (
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date={item.date}
-                    icon={item.icon}
-                    key={item.id}
-                >
-                    <h2 className="vertical-timeline-element-title">
-                        <strong>{item.title}</strong>
-                    </h2>
-                    <h4 className="vertical-timeline-element-subtitle">{item.company}</h4>
-                    <p className={"vertical-timeline-element-description"}>
-                        {item.description}
-                    </p>
-                        <Tags id={item.id}
-                              tags={item.tags}>
-                        </Tags>
+  const { items } = props || {};
+  return (
+    <VerticalTimeline>
+      {items.map((item) => (
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date={item.date}
+          icon={item.icon}
+          key={item.id}
+        >
+          <h2 className="vertical-timeline-element-title">
+            <strong>{item.title}</strong>
+          </h2>
+          <h4 className="vertical-timeline-element-subtitle">{item.company}</h4>
+          <p className={"vertical-timeline-element-description"}>
+            {item.description}
+          </p>
+          <Tags id={item.id} tags={item.tags}></Tags>
+        </VerticalTimelineElement>
+      ))}
+    </VerticalTimeline>
+  );
+};
 
-                </VerticalTimelineElement>
-            ))
-            }
-        </VerticalTimeline>
-    )
-
-}
-
-export default Timeline
+export default Timeline;
