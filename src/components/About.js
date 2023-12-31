933f6638306bb31157685e2d5e0235cb528dcf7a
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-12-31 23:16:05 +0500

    optimize code

--- src/components/About.js
+++ src/components/About.js
@@ -1,49 +1,49 @@
 import React from "react";
 
 const About = (props) => {
-    const about = props.about.start
-    const aboutExit = props.about.exit
+  const { about } = props || {};
+  const { start, exit } = about || {};
 
-    return (
-        <section id="about">
-            <div className="about-card" style={{backgroundColor: "#263238"}}>
-                <div className="card-header">
-                    <span
-                        className="iconify"
-                        data-icon="emojione:red-circle"
-                        data-inline="false"
-                    />{" "}
-                    &nbsp;{" "}
-                    <span
-                        className="iconify"
-                        data-icon="twemoji:yellow-circle"
-                        data-inline="false"
-                    />{" "}
-                    &nbsp;{" "}
-                    <span
-                        className="iconify"
-                        data-icon="twemoji:green-circle"
-                        data-inline="false"
-                    />
-                </div>
-                <div
-                    className="card-body font-trebuchet text-justify ml-3 mr-3"
-                    style={{
-                        height: "auto",
-                        fontSize: "132%",
-                        lineHeight: "150%",
-                        backgroundColor: "#F6F6F6",
-                        color: "#8e8d8a",
-                        padding: "2rem"
-                    }}
-                >
-                    {about}
-                    <br/>
-                    {aboutExit}
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="about">
+      <div className="about-card" style={{ backgroundColor: "#263238" }}>
+        <div className="card-header">
+          <span
+            className="iconify"
+            data-icon="emojione:red-circle"
+            data-inline="false"
+          />{" "}
+          &nbsp;{" "}
+          <span
+            className="iconify"
+            data-icon="twemoji:yellow-circle"
+            data-inline="false"
+          />{" "}
+          &nbsp;{" "}
+          <span
+            className="iconify"
+            data-icon="twemoji:green-circle"
+            data-inline="false"
+          />
+        </div>
+        <div
+          className="card-body font-trebuchet text-justify ml-3 mr-3"
+          style={{
+            height: "auto",
+            fontSize: "132%",
+            lineHeight: "150%",
+            backgroundColor: "#F6F6F6",
+            color: "#8e8d8a",
+            padding: "2rem",
+          }}
+        >
+          {start}
+          <br />
+          {exit}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
