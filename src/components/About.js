91477b139fd87c22c74a34d7d95a1e2f31cda3c2
Amjad Saeed <amjadsaeedmail@gmail.com>
2024-01-01 16:08:53 +0500

    optimize code

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,24 @@ const About = (props) => {
   const { about } = props || {};
   const { start, exit } = about || {};
 
+  const aboutCardStyle = {
+    backgroundColor: "#263238",
+  };
+
+  const cardHeaderStyle = {};
+
+  const cardBodyStyle = {
+    height: "auto",
+    fontSize: "132%",
+    lineHeight: "150%",
+    backgroundColor: "#F6F6F6",
+    color: "#8e8d8a",
+    padding: "2rem",
+  };
+
   return (
     <section id="about">
-      <div className="about-card" style={{ backgroundColor: "#263238" }}>
+      <div className="about-card" style={aboutCardStyle}>
         <div className="card-header">
           <span
             className="iconify"
@@ -28,14 +43,7 @@ const About = (props) => {
         </div>
         <div
           className="card-body font-trebuchet text-justify ml-3 mr-3"
-          style={{
-            height: "auto",
-            fontSize: "132%",
-            lineHeight: "150%",
-            backgroundColor: "#F6F6F6",
-            color: "#8e8d8a",
-            padding: "2rem",
-          }}
+          style={cardBodyStyle}
         >
           {start}
           <br />
